a663a8f154839abe3be078640ca32a18ef01e3ed
Archit Pandey <architpandeynitk@gmail.com>
2020-06-18 18:23:50 +0530

    fixup! fixup! aqm: add CoDel AQM
    Signed-off-by: Archit Pandey <architpandeynitk@gmail.com>

--- lib/librte_aqm/aqm_codel.c
+++ lib/librte_aqm/aqm_codel.c
@@ -11,10 +11,40 @@
 
 #include "aqm_codel.h"
 
-struct aqm_codel {
-	struct rte_codel_rt codel_rt;
-	struct rte_codel_config codel_config;
-};
+/*
+ * Run a Newton method step:
+ * new_invsqrt = (invsqrt / 2) * (3 - count * invsqrt^2)
+ *
+ * Here, invsqrt is a fixed point number (< 1.0), 32bit mantissa, aka Q0.32
+ */
+
+/**
+ * @brief Newton step function
+ *
+ * @param codel [in,out] data pointer to CoDel runtime data
+ */
+static void
+codel_newton_step(struct rte_codel_rt *codel);
+
+/**
+ * @brief CoDel Control Law
+ *
+ * @param t [in] current drop time of the packet
+ * @param interval [in] sliding time window width
+ * @param rec_inv_sqrt [in] reciprocal value of sqrt(count) >> 1
+ *
+ * @return Next drop time of the packet
+ */
+static u_int64_t 
+codel_control_law(u_int64_t t, 
+	u_int64_t interval, 
+	u_int32_t rec_inv_sqrt);
+
+int
+aqm_codel_drop(struct rte_codel_rt *codel, 
+	struct rte_codel_config *config, 
+	uint32_t qlen, 
+	uint64_t timestamp);
 
 size_t aqm_codel_get_memory_size(void)
 {
@@ -24,14 +54,18 @@ size_t aqm_codel_get_memory_size(void)
 int aqm_codel_init(struct aqm_codel *codel, struct rte_aqm_codel_params *params)
 {
 	struct rte_codel_params *codel_params = &params->params;
+	struct rte_codel_config *config = &codel->codel_config;
 
-	if (rte_codel_config_init(&codel->codel_config,
-				codel_params->target,
-				codel_params->interval)) {
-		RTE_LOG(ERR, AQM, "%s: configuration initiliazation failure\n",
-				__func__);
+	if (config == NULL)
 		return -1;
-	}
+	if (codel_params->target <= 0)
+		return -2;
+	if (codel_params->interval <= 0)
+		return -3;
+
+	uint64_t cycles 		= rte_get_timer_hz();
+	config->target 			= codel_params->target * cycles / 1000u;	//5000ul = 5ms in microseconds
+	config->interval		= codel_params->interval * cycles / 1000u;	//10000ul = 100ms in microseconds
 
 	return 0;
 }
@@ -42,6 +76,82 @@ int aqm_codel_enqueue(struct aqm_codel *codel, struct circular_queue *cq,
 	return circular_queue_enqueue(cq, pkt);
 }
 
+static void 
+codel_newton_step(struct rte_codel_rt *codel)
+{
+	uint32_t invsqrt, invsqrt2;
+	uint64_t val;
+
+	invsqrt = ((u_int32_t)codel->rec_inv_sqrt) << REC_INV_SQRT_SHIFT;
+	invsqrt2 = ((u_int64_t)invsqrt * invsqrt) >> 32;
+	val = (3LL << 32) - ((u_int64_t)codel->count * invsqrt2);
+	val >>= 2; /* avoid overflow in following multiply */
+	val = (val * invsqrt) >> (32 - 2 + 1);
+	codel->rec_inv_sqrt = val >> REC_INV_SQRT_SHIFT;
+}
+
+static u_int64_t
+codel_control_law(u_int64_t t, 
+	u_int64_t interval, 
+	u_int32_t rec_inv_sqrt)
+{
+	return (t + (u_int32_t)(((u_int64_t)interval *
+	    (rec_inv_sqrt << REC_INV_SQRT_SHIFT)) >> 32));
+}
+
+int
+aqm_codel_drop(struct rte_codel_rt *codel, 
+	struct rte_codel_config *config, 
+	uint32_t qlen, 
+	uint64_t timestamp)
+{
+	if (qlen == 0) {
+		codel->first_above_time = 0;	//Since queue is empty, there is nothing to dequeue.
+	}
+	uint64_t now = rte_get_tsc_cycles();
+	codel->sojourn_time = (now - timestamp);
+	uint32_t delta;
+	codel->ok_to_drop = CODEL_DEQUEUE;
+
+	if ((codel->sojourn_time < config->target) || (qlen <= MAX_PACKET)) {
+		codel->first_above_time = 0;	//Since sojourn time is less than TARGET, stay down for atleast INTERVAL
+	} else {
+		if (codel->first_above_time == 0) {
+			codel->first_above_time = now + config->interval;
+		} else if (now >= codel->first_above_time) {
+			codel->ok_to_drop = CODEL_DROP;
+		}
+	}
+
+	if (codel->dropping_state) {
+		if (codel->ok_to_drop == CODEL_DEQUEUE) {
+			codel->dropping_state = false;	// sojourn time below TARGET - leave drop state
+		}
+		while (now >= codel->drop_next && codel->dropping_state) {
+			++codel->count;
+			codel_newton_step(codel);
+			if (!codel->ok_to_drop) {
+				codel->dropping_state = false;
+			} else {
+				codel->drop_next = codel_control_law(codel->drop_next, config->interval, codel->rec_inv_sqrt);
+			}
+		}
+	} else if (codel->ok_to_drop == CODEL_DROP) {
+		codel->dropping_state = true;
+		delta = codel->count - codel->lastcount;
+		codel->count = 1;
+		if ((delta > 1) && (now - codel->drop_next < 16*config->interval)) {
+			codel->count = delta;
+			codel_newton_step(codel);
+		} else {
+			codel->rec_inv_sqrt =  ~0U >> REC_INV_SQRT_SHIFT;
+		}
+		codel->drop_next = codel_control_law(now, config->interval, codel->rec_inv_sqrt);
+		codel->lastcount = codel->count;
+	}
+	return codel->ok_to_drop;
+}
+
 int aqm_codel_dequeue(struct aqm_codel *codel, struct circular_queue *cq,
 			  struct rte_mbuf **pkt, uint16_t *n_pkts_dropped,
 			  uint32_t *n_bytes_dropped)
@@ -49,6 +159,7 @@ int aqm_codel_dequeue(struct aqm_codel *codel, struct circular_queue *cq,
 	int ret = 0;
 	uint16_t qlen = circular_queue_get_length_pkts(cq);
 	ret = circular_queue_dequeue(cq, pkt);
+
 	if (unlikely(ret)) {
 		RTE_LOG(ERR, AQM, "%s: dequeue failure\n", __func__);
 		return -1;
@@ -56,7 +167,7 @@ int aqm_codel_dequeue(struct aqm_codel *codel, struct circular_queue *cq,
 	*n_pkts_dropped = 0;
 	*n_bytes_dropped = 0;
 
-	if (unlikely(rte_codel_dequeue(&codel->codel_rt,
+	if (unlikely(aqm_codel_drop(&codel->codel_rt,
 				&codel->codel_config,
 				qlen,
 				(*pkt)->timestamp))) {
--- lib/librte_aqm/aqm_codel.h
+++ lib/librte_aqm/aqm_codel.h
@@ -21,7 +21,38 @@
 extern "C" {
 #endif
 
-struct aqm_codel;
+#define CODEL_DROP 				1	/**< Return status variable: DROP drop/discard the packet */
+#define CODEL_DEQUEUE 			0	/**< Return status variable: DEQUEUE dequeque the packet from queue */
+#define MAX_PACKET 				512	/**< Maximum packet size in bytes (SHOULD use interface MTU) */
+#define	REC_INV_SQRT_BITS 		(8 * sizeof(u_int16_t))		/**< sizeof_in_bits (rec_inv_sqrt) */
+#define	REC_INV_SQRT_SHIFT 		(32 - REC_INV_SQRT_BITS)	/**< Needed shift to get a Q0.32 number from rec_inv_sqrt */
+
+/**	
+ * CoDel runtime data
+ */
+struct rte_codel_rt {
+	uint32_t	count;			/**< Number of packets dropped since last dropping state */
+	uint32_t	lastcount;		/**< Count when going into dropping state */
+	int 		ok_to_drop;		/**< Flag to check whether to drop or not */
+	bool 		dropping_state;		/**< Set true if CoDel is in dropping state */
+	uint16_t 	rec_inv_sqrt;   	/**< For control_law: reciprocal value of sqrt(count) >> 1 */
+	uint64_t	first_above_time;	/**< Time when sojourn time goes continuously above target */
+	uint64_t	drop_next;		/**< Time when we should drop the next packet */
+	uint64_t	sojourn_time;		/**< Sojourn time as in queue delay */
+};
+
+/**
+ * CoDel configuration parameters
+ */
+struct rte_codel_config {
+	uint64_t	target;			/**< target */
+	uint64_t	interval;		/**< interval */
+};
+
+struct aqm_codel {
+	struct rte_codel_rt codel_rt;
+	struct rte_codel_config codel_config;
+};
 
 size_t aqm_codel_get_memory_size(void);
 
